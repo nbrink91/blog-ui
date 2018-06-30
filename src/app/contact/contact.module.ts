654f231008779b038b71c0b01f255f9008fbb2a8
Nicholas Brink <nbrink7@gmail.com>
2018-06-29 23:14:21 -0500

    fix: remove references to font awesome

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -8,12 +8,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ContactComponent } from './contact.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { MatDividerModule } from '@angular/material/divider';
-
-library.add(faEnvelope);
 
 @NgModule({
   imports: [
@@ -25,7 +20,6 @@ library.add(faEnvelope);
     MatSnackBarModule,
     FlexLayoutModule,
     ReactiveFormsModule,
-    FontAwesomeModule,
     MatDividerModule
   ],
   exports: [
